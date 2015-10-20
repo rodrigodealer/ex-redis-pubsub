062b0fde67ec6e26c63a6eba1a1e81f0a0cd13da
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-10-19 23:23:30 -0200

    Heroku

--- apps/publisher/config/config.exs
+++ apps/publisher/config/config.exs
@@ -4,9 +4,9 @@ config :maru, Server.API,
   http: [port: 8880]
 
 config :exredis,
-  host: "192.168.99.100",
-  port: 6379,
-  password: "",
+  host: System.get_env("REDIS_HOST"),#"192.168.99.100",
+  port: System.get_env("REDIS_PORT"),
+  password: System.get_env("REDIS_PASSWORD"),
   db: 0,
   reconnect: :no_reconnect,
   max_queue: :infinity
--- apps/subscriber/config/config.exs
+++ apps/subscriber/config/config.exs
@@ -1,9 +1,9 @@
 use Mix.Config
 
 config :exredis,
-  host: "192.168.99.100",
-  port: 6379,
-  password: "",
+  host: System.get_env("REDIS_HOST"),
+  port: System.get_env("REDIS_PORT"),
+  password: System.get_env("REDIS_PASSWORD"),
   db: 0,
   reconnect: :no_reconnect,
   max_queue: :infinity
