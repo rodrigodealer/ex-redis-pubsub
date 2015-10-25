00e3870dd580347eca12ec29e76937017564686e
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-10-25 17:28:29 -0200

    For each received message, a new process is fired.

--- apps/subscriber/lib/client/redis_client.ex
+++ apps/subscriber/lib/client/redis_client.ex
@@ -3,15 +3,7 @@ defmodule Server.API.Subscriber.Client.RedisClient do
     IO.puts("RedisClient initialized")
     client_sub = Exredis.Sub.start
     client_sub |> Exredis.Sub.subscribe "queue", fn(msg) ->
-      spawn fn -> read(msg) end
-    end
-  end
-
-  def read(message) do
-    if elem(message, 0) == :message do
-      {_, _, json, _} = message
-      IO.puts "Received json"
-      IO.inspect Poison.decode!(json, as: Server.API.Models.Message)
+      spawn fn -> Server.API.Subscriber.Processor.QueueProcessor.process(msg) end
     end
   end
 end
